7a7db9b795503943ef9fbaeb9569c0dcc50f9cb8
Kostya <koooooostaaa@gmail.com>
2023-07-16 18:49:25 +0300

    create new unit tests

---
+++ tests/Feature/TravelsListTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TravelsListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_travels_list_returns_paginated_data_correctly(): void
+    {
+        Travel::factory(16)->create(['is_public' => true]);
+
+        $response = $this->get('/api/v1/travels');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(15, 'data');
+        $response->assertJsonPath('meta.last_page', 2);
+    }
+
+    public function test_travels_list_shows_only_public_records()
+    {
+        $publicTravel = Travel::factory()->create(['is_public' => true]);
+        Travel::factory()->create(['is_public' => false]);
+
+        $response = $this->get('/api/v1/travels');
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1, 'data');
+        $response->assertJsonPath('data.0.name', $publicTravel->name);
+    }
+}
