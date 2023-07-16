41912c723023db22cf0ff5a115c9dda3176d0a3a
Kostya <koooooostaaa@gmail.com>
2023-07-16 18:51:21 +0300

    create new controllers

---
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TourResource;
+use App\Models\Tour;
+use App\Models\Travel;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class TourController extends Controller
+{
+    public function index(Travel $travel): AnonymousResourceCollection
+    {
+        $tours = $travel->tours()
+            ->latest('starting_date')
+            ->paginate(Tour::PER_PAGE);
+
+        return TourResource::collection($tours);
+    }
+}
