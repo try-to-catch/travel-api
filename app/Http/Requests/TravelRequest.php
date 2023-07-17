78e48ca43fa6f14c73474d62e20761dd5dd12552
Kostya <koooooostaaa@gmail.com>
2023-07-17 19:29:36 +0300

    create new FormRequests

---
+++ app/Http/Requests/TravelRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class TravelRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'is_public' => ['boolean'],
+            'name' => ['required', 'unique:travels', 'string'],
+            'description' => ['required', 'string'],
+            'number_of_days' => ['required', 'integer'],
+        ];
+    }
+}
